b74eb93189f30fd058db35ba61e44f277f059c62
Ruchit <nigam21ruchit@gmail.com>
2023-12-19 16:56:11 +0530

    Configure React app for deployment to GitHub Pages

---
+++ app/Form/form.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+
+function form() {
+
+    const [title,setTitle]= useState("");
+    const [desc,setDesc] = useState("");
+    const [mainTask,setMainTask]= useState([]);
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+
+        setMainTask([...mainTask,{title, desc}]);
+        
+        setTitle("");
+        setDesc("");
+
+        console.log(mainTask);
+    }
+
+    const deleteHandler= (i)=>{
+        let copyTask = [...mainTask]
+    
+        copyTask.splice(i,1);
+        setMainTask(copyTask)
+      }
+    
+      let renderTask = "No Task Available";
+    
+      if (mainTask.length>0)
+      {
+        renderTask = mainTask.map((t,i)=>{
+          return(
+            <li key={i} className='flex items-center justify-between'>
+              <div className='flex justify-between mb-5 w-2/3'>
+                <h5 className='text-xl font-semibold'>{i+1}</h5>
+                <h5 className='text-2xl font-semibold'>{t.title}</h5>
+                <h6 className='text-xl font-semibold'>{t.desc}</h6>
+    
+                <button onClick={()=>{
+                deleteHandler(i)}}
+                className='bg-red-600 text-white font-bold px-3 rounded'>Delete</button>
+              </div>
+            </li>
+          
+        )});
+      }
+
+    return (
+        <>
+            <form>
+                <input type="text" onChange={(e)=>{
+                // console.log(e.target.value);
+                setTitle(e.target.value);
+                }} className='text-2xl border-zinc-800 border-4 m-4 px-4 py-2'
+                placeholder="Enter Task Here"
+                value={title}></input>
+
+                <input type="text" onChange={(e)=>{
+                setDesc(e.target.value);
+                }}
+                className='text-2xl border-zinc-800 border-4 px-4 m-4 py-2'
+                placeholder='Enter Task Description' value={desc}></input>
+
+                <button onClick={submitHandler}
+                className='bg-green-500 p-3 mx-8 text-white font-bold text-xl rounded'>Add Task</button>
+            </form>
+
+            <br />
+            <div className='p-8 bg-slate-200'>
+                <ul>
+                {renderTask}
+                </ul>
+            </div>
+                </>
+    );
+}
+
+export default form;
---
+++ app/Navbar/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function navbar() {
+    return (
+        <>
+            <h1 className='bg-black text-white p-5 text-4xl font-bold text-center'>Ruchit's Todo List</h1>
+        </>
+    );
+}
+
+export default navbar;
