c410dfd2060ae897d5577d3460f94f5dc87a36c1
Ruchit <nigam21ruchit@gmail.com>
2023-12-24 10:32:17 +0530

    First commit

--- Components/form.js
+++ Components/form.js
@@ -1,10 +1,38 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function form() {
 
     const [title,setTitle]= useState("");
     const [desc,setDesc] = useState("");
     const [mainTask,setMainTask]= useState([]);
+
+    const notify = () =>{
+      toast.success('Task Added Successfully', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    }
+
+    const deleteNotify = ()=>{
+      toast.error('Task Removed Successfully', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    }
 
     const submitHandler = (e)=>{
         e.preventDefault();
@@ -15,6 +43,7 @@ function form() {
         setDesc("");
 
         console.log(mainTask);
+        notify();
     }
 
     const deleteHandler= (i)=>{
@@ -22,6 +51,7 @@ function form() {
     
         copyTask.splice(i,1);
         setMainTask(copyTask)
+        deleteNotify();
       }
     
       let renderTask = "No Task Available";
@@ -44,6 +74,7 @@ function form() {
           
         )});
       }
+      
 
     return (
         <>
--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
 'use client'
 import React, { useState } from 'react';
-import Navbar from "@/app/Navbar/navbar";
-import Form from "@/app/Form/form";
+import Navbar from "@/Components/navbar";
+import Form from "@/Components/form";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function page() {
+  
   return (
     <>
       <Navbar />
       <Form />
+      <ToastContainer />
     </>
   );
 }
